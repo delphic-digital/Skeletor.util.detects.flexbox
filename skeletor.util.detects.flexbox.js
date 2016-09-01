e7726070599d7920b57301a9680573445f5abf27
Jeff <jmills@delphicdigital.com>
2016-09-01 17:01:41 -0400

    Simple flexbox detection

--- skeletor.util.detects.flexbox.js
+++ skeletor.util.detects.flexbox.js
@@ -15,8 +15,15 @@ define(['jquery', 'skeletor.core'],function($, Skeletor, isMobile){
 
 	Skeletor.Plugin.create(DetectFlexbox, {
 		_init: function(element) {
-
-
+			var c = " ", prefixFlex = 'flex -webkit-flex -ms-flexbox -moz-box -webkit-box'.split(' '), elem = document.createElement('b'), mStyle = elem.style;
+			try {
+				for (var i = 0, len = prefixFlex.length; i < len; i++) {
+					c += ( (mStyle.display = prefixFlex[i]) && mStyle.display != prefixFlex[i] ) ? 'no-flexbox' :  'flexbox', (i = len);
+				}
+			} catch(e) {
+				c += "no-flexbox";
+			}
+			document.documentElement.className =  c;
 		}
 	});
 
